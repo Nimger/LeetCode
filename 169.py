936e61d818a465e79cdf7efbc7d7473a70a893ab
Nimger <zhengzhilin@nimger.com>
2017-08-05 17:11:31 +0800

    majorityElement

---
+++ 169.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+class Solution(object):
+	def majorityElement(self, nums):
+		"""
+        :type nums: List[int]
+        :rtype: int
+        """
+		count = 1
+		element = nums[0]
+		if len(nums) <= 1:
+			return element
+		for num in nums[1:]:
+			if count == 0:
+				element = num
+				count = 1
+			else:
+				if element == num:
+					count += 1
+				else:
+					count -= 1
+		return element
+
+
+if __name__ == '__main__':
+	solution = Solution()
+	print(solution.majorityElement([1]))
