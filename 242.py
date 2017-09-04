404af1678b67de102fe457b5a6c9a70ebbb89f64
Nimger <zhengzhilin@nimger.com>
2017-09-04 14:07:56 +0800

    isAnagram

---
+++ 242.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
+
+if __name__ == '__main__':
+	solution = Solution()
+	print(solution.isAnagram('',''))
